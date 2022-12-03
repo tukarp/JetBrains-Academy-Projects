be4a751d80481035bea8470593ddf1cf97fef7a7
tukarp <114019481+tukarp@users.noreply.github.com>
2022-12-03 20:56:01 +0100

    03.12.22

--- Loan Calculator.py
+++ Loan Calculator.py
@@ -6,28 +6,44 @@ import argparse
 
 
 # creating parser
-parser = argparse.ArgumentParser(description="Loan Calculator")
+def create_parser():
+    parser = argparse.ArgumentParser(description="Loan Calculator")
 
-# parser arguments
-parser.add_argument("--type", help = "Annuity or Differential types of payments", choices = ["annuity", "diff"])
-parser.add_argument("--payment", help = "Monthly payment", type = int)
-parser.add_argument("--principal", help = "Loan principal", type = int)
-parser.add_argument("--", help = "Number of months", type = int)
-parser.add_argument("--interest", help = " ", type = float)
-args = parser.parse_args()
-args_list = [args.type, args.payment, args.principal, args.periods, args.interest]
+    # parser arguments
+    parser.add_argument("--type", help="Annuity or Differential types of payments")
+    parser.add_argument("--payment", help="Monthly payment", type=int)
+    parser.add_argument("--principal", help="Loan principal", type=int)
+    parser.add_argument("--periods", help="Number of months", type=int)
+    parser.add_argument("--interest", help="Rate of interest", type=float)
+    args = parser.parse_args()
 
-# checking conditions to choose function
-if args.type != ["annuity", "diff"]:
-    print("Incorrect parameters")
-    exit()
-else:
-    # case for annuity payments
-    if args.type == "annuity":
-        pass
-    # case for differentiated payments
+
+    # checking conditions to choose function
+    if args.type == "diff" and args.payment is not None:
+        print("Incorrect parameters")
+        exit(0)
+    elif args.type not in ["annuity", "diff"]:
+        print("Incorrect parameters")
+        exit(0)
     else:
-        pass
+        # case for annuity payments
+        if args.type == "annuity":
+            # case for n
+            if (args.principal is not None) and (args.payment is not None) and (args.interest is not None):
+                number_of_monthly_payments_calculator(args.principal, args.payment, args.interest)
+            # case for a
+            elif (args.principal is not None) and (args.periods is not None) and (args.interest is not None):
+                annuity_monthly_payment_calculator(args.principal, args.periods, args.interest)
+            # case for p
+            elif (args.payment is not None) and (args.periods is not None) and (args.interest is not None):
+                loan_principal_calculator(args.payment, args.periods, args.interest)
+        # case for differentiated payments
+        else:
+            # case for d
+            if (args.principal is not None) and (args.periods is not None) and (args.interest is not None):
+                differentiated_payments_calculator(args.principal, args.periods, args.interest)
+            else:
+                print("Incorrect parameters")
 
 
 # program loop
@@ -37,18 +53,31 @@ def start_program():
         print("type 'n' - for number of monthly payments,")
         print("type 'a' - for annuity monthly payment amount,")
         print("type 'p' - for the monthly payment,")
-        # print("type 'exit' - to exit the program:")
+        print("type 'd' - for differentiated payments,")
+        print("type 'exit' - to exit the program:")
         user_input = input()
         # cases
         match user_input:
-            case "n":
-                number_of_monthly_payments_calculator()
+            case "m":
+                loan_principal = get_loan_principal()
+                monthly_payment = int(input("Enter the monthly payment: \n"))
+                loan_interest = get_loan_interest()
+                number_of_monthly_payments_calculator(loan_principal, monthly_payment, loan_interest)
             case "a":
-                annuity_monthly_payment_calculator()
+                loan_principal = get_loan_principal()
+                periods = get_number_of_periods()
+                loan_interest = get_loan_interest()
+                annuity_monthly_payment_calculator(loan_principal, periods, loan_interest)
             case "d":
-                differentiated_payments_calculator()
+                loan_principal = get_loan_principal()
+                periods = get_number_of_periods()
+                loan_interest = get_loan_interest()
+                differentiated_payments_calculator(loan_principal, periods, loan_interest)
             case "p":
-                loan_principal_calculator()
+                annuity_payment = float(input("Enter the annuity payment: \n"))
+                periods = get_number_of_periods()
+                loan_interest = get_loan_interest()
+                loan_principal_calculator(annuity_payment, periods, loan_interest)
             case "exit":
                 exit()
             case _:
@@ -80,17 +109,7 @@ def get_overpayment(paid, loan_principal):
     return int(overpayment)
 
 
-# case n
-def number_of_monthly_payments_calculator():
-    # asking for loan principal
-    loan_principal = get_loan_principal()
-
-    # asking for monthly payment
-    monthly_payment = int(input("Enter the monthly payment: \n"))
-
-    # asking for loan interest
-    loan_interest = get_loan_interest()
-
+def number_of_monthly_payments_calculator(loan_principal, monthly_payment, loan_interest):
     # calculating months to repay the loan and rounding the result
     months_to_repay = math.ceil(
         math.log((monthly_payment / (monthly_payment - (loan_interest * loan_principal))), 1 + loan_interest))
@@ -106,32 +125,23 @@ def number_of_monthly_payments_calculator():
 
     # cases for multiple or single months and years
     if years_to_repay > 1 and months_to_repay > 1:
-        print(f"It will take {years_to_repay} years and {months_to_repay} months to repay the loan!")
+        print(f"It will take {years_to_repay} years and {months_to_repay} months to repay the loan")
     elif years_to_repay == 1 and months_to_repay == 1:
-        print(f"It will take {years_to_repay} year and {months_to_repay} month to repay the loan!")
+        print(f"It will take {years_to_repay} year and {months_to_repay} month to repay the loan")
     elif years_to_repay > 1 and months_to_repay == 0:
-        print(f"It will take {years_to_repay} years to repay the loan!")
+        print(f"It will take {years_to_repay} years to repay the loan")
     elif years_to_repay == 1 and months_to_repay == 0:
-        print(f"It will take {years_to_repay} year to repay the loan!")
+        print(f"It will take {years_to_repay} year to repay the loan")
     elif years_to_repay == 0 and months_to_repay > 1:
-        print(f"It will take {months_to_repay} months to repay the loan!")
+        print(f"It will take {months_to_repay} months to repay the loan")
     else:
-        print(f"It will take {months_to_repay} month to repay the loan!")
+        print(f"It will take {months_to_repay} month to repay the loan")
+
     # printing overpayment
-    print(f"Overpayment = {overpayment}!")
+    print(f"Overpayment = {overpayment}")
 
 
-# case a
-def annuity_monthly_payment_calculator():
-    # asking for loan principal
-    loan_principal = get_loan_principal()
-
-    # asking for number of periods
-    periods = get_number_of_periods()
-
-    # asking for loan interest
-    loan_interest = get_loan_interest()
-
+def annuity_monthly_payment_calculator(loan_principal, periods, loan_interest):
     # calculating payment for every month
     annuity_payment = math.ceil(loan_principal * ((loan_interest * math.pow((1 + loan_interest), periods)) / (
                 math.pow((1 + loan_interest), periods) - 1)))
@@ -140,48 +150,12 @@ def annuity_monthly_payment_calculator():
     overpayment = get_overpayment(annuity_payment * periods, loan_principal)
 
     # printing monthly payment and overpayment
-    print(f"Your monthly payment = {annuity_payment}!")
-    print(f"Overpayment = {overpayment}!")
+    print(f"Your monthly payment = {annuity_payment}")
+    print(f"Overpayment = {overpayment}")
 
 
-# case d
-def differentiated_payments_calculator():
-    # asking for loan principal
-    loan_principal = get_loan_principal()
-
-    # asking for number of periods
-    periods = get_number_of_periods()
-
-    # asking for loan interest
-    loan_interest = get_loan_interest()
-
-    # calculating differentiated payments
-    overpayment = 0
-    month_counter = 1
-    while month_counter < (periods + 1):
-        differentiated_payments = math.ceil((loan_principal / periods) + (loan_interest * (loan_principal - (
-                    loan_principal * (month_counter - 1) / periods))))
-        # printing payments for every month
-        print(f"Month {month_counter}: payment is {differentiated_payments}")
-        overpayment += differentiated_payments
-        month_counter += 1
-
-    # calculating and printing overpayment
-    overpayment = get_overpayment(overpayment, loan_principal)
-    print(f"Overpayment = {overpayment}!")
-
-
-# case p
-def loan_principal_calculator():
-    # asking for annuity payment
-    annuity_payment = float(input("Enter the annuity payment: \n"))
-
-    # asking for number of periods
-    periods = get_number_of_periods()
-
-    # asking for loan interest
-    loan_interest = get_loan_interest()
-
+# calculating loan principal
+def loan_principal_calculator(annuity_payment, periods, loan_interest):
     # calculating loan principal
     loan_principal = math.floor(annuity_payment / ((loan_interest * math.pow((1 + loan_interest), periods)) / (
                 math.pow((1 + loan_interest), periods) - 1)))
@@ -190,9 +164,27 @@ def loan_principal_calculator():
     overpayment = get_overpayment(annuity_payment * periods, loan_principal)
 
     # printing loan principal and overpayment
-    print(f"Your loan principal = {loan_principal}!")
-    print(f"Overpayment = {overpayment}!")
+    print(f"Your loan principal = {loan_principal}")
+    print(f"Overpayment = {overpayment}")
+
+
+# calculating differentiated payments
+def differentiated_payments_calculator(loan_principal, periods, loan_interest):
+    overpayment = 0
+    month_counter = 1
+    while month_counter < (periods + 1):
+        differentiated_monthly_payment = math.ceil((loan_principal / periods) + (loan_interest * (loan_principal - (
+                    loan_principal * (month_counter - 1) / periods))))
+        # printing payments for every month
+        print(f"Month {month_counter}: payment is {differentiated_monthly_payment}")
+        overpayment += differentiated_monthly_payment
+        month_counter += 1
+
+    # calculating and printing overpayment
+    overpayment = get_overpayment(overpayment, loan_principal)
+    print(f"Overpayment = {overpayment}")
 
 
 # starting program and choosing program's feature
-start_program()
+create_parser()
+# start_program()
