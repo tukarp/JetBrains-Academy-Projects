61b990d526d9091dc7b637cf68e4226dc168061f
tukarp <114019481+tukarp@users.noreply.github.com>
2022-12-06 21:25:54 +0100

    Done, 06.12.22

--- Last Pencil.py
+++ Last Pencil.py
@@ -3,6 +3,7 @@
 
 # getting the number of pencils
 def get_pencils():
+    print("How many pencils would you like to use: ")
     while True:
         number_of_pencils = input()
         # if number of pencils is digit
@@ -17,22 +18,25 @@ def get_pencils():
 
 
 # getting the first player
-def get_first_player(players_list):
+def get_first_player(players):
+    print(f"Who will be the first ({players[0]}, {players[1]}): ")
     while True:
         first_player = input()
-        if first_player not in players_list:
-            print(f"Choose between' {players_list[0]}' and '{players_list[1]}'")
-        # returning player1 or player2
-        elif first_player == players_list[0]:
-            return 0
-        elif first_player == players_list[1]:
-            return 1
+        if first_player not in players:
+            print(f"Choose between' {players[0]}' and '{players[1]}'")
+        else:
+            # returning player1 or player2
+            if first_player == players[0]:
+                return 0
+            elif first_player == players[1]:
+                return 1
 
 
 # checking if player move is correct
-def move(number_of_pencils, players_list, player_turn):
+def move(pencils, players, turn):
     # player move
-    if player_turn == 0:
+    if turn == 0:
+        print(f"{players[turn]}'s turn: ")
         while True:
             player_move = input()
             # if number of pencils is digit
@@ -40,7 +44,7 @@ def move(number_of_pencils, players_list, player_turn):
                 # if number of pencils is 1 or 2 or 3
                 if player_move in ("1", "2", "3"):
                     # if number of pencils taken isn't bigger than pencils left
-                    if number_of_pencils - int(player_move) >= 0:
+                    if pencils - int(player_move) >= 0:
                         return int(player_move)
                     else:
                         print("Too many pencils were taken")
@@ -51,44 +55,44 @@ def move(number_of_pencils, players_list, player_turn):
     # bot move
     else:
         # bot strategies for winning
-        bot_move = 0
-        if number_of_pencils > 5:
-            print(f"{players_list[player_turn]}'s turn: ")
-            if number_of_pencils % 4 == 0:
-                bot_move = 3
-            elif number_of_pencils % 4 == 3:
-                bot_move = 2
-            elif number_of_pencils % 4 == 2:
-                bot_move = 1
-            print(bot_move)
-            return int(bot_move)
+        print(f"{players[turn]}'s turn: ")
+        if pencils % 4 == 0:
+            bot_move = 3
+        elif pencils % 4 == 3:
+            bot_move = 2
+        elif pencils % 4 == 2:
+            bot_move = 1
         else:
-            if number_of_pencils == 5:
-                print("2 for Jack \n" + "1 for Jack's move \n" + "1 for game-results")
-                quit()
+            bot_move = 1
+        print(bot_move)
+        return bot_move
 
 
 # game loop
-def game(number_of_pencils, players_list, player_turn):
-    while number_of_pencils > 0:
-        print("|" * number_of_pencils)
-        print(f"{players_list[player_turn]}'s turn: ")
-        number_of_pencils -= move(number_of_pencils, players_list, player_turn)
-        player_turn ^= 1
-    print(f"{players_list[player_turn]} won!")
+def game(pencils, players, turn):
+    while pencils > 0:
+        print("|" * pencils)
+        pencils -= move(pencils, players, turn)
+        turn ^= 1
+    print(f"{players[turn]} won!")
 
 
-# declaring players
-player_name = input("What's your name? \n")
-players = [player_name, "Bot"]
+# start game
+def start_game():
+    # declaring players
+    player_name = input("What's your name? \n")
+    bot_name = "Bot"
+    players = [player_name, bot_name]
 
-# getting the number of pencils
-print("How many pencils would you like to use: ")
-pencils = get_pencils()
+    # getting the number of pencils
+    pencils = get_pencils()
 
-# declaring the starting player
-print(f"Who will be the first ({players[0]}, {players[1]}): ")
-turn = get_first_player(players)
+    # declaring the starting player
+    turn = get_first_player(players)
 
-# game
-game(pencils, players, turn)
+    # game
+    game(pencils, players, turn)
+
+
+# starting game
+start_game()
