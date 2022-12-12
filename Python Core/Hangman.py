60d449b6198d1a810a88bac7733cd7907d43300e
tukarp <114019481+tukarp@users.noreply.github.com>
2022-12-12 18:07:48 +0100

    Done

--- Python Core/Hangman.py
+++ Python Core/Hangman.py
@@ -9,11 +9,11 @@ import random
 def print_hangman(attempts):
     # hangman at 8 attempts left
     hangman_8 = """
-                            
-                            
-                            
-                            
-                            
+
+
+
+
+
                    ---
     """
     # hangman at 7 attempts left
@@ -95,7 +95,7 @@ def print_hangman(attempts):
                     |      / \     
                    ---          
     """
-    
+
     # list with hangman versions for every attempt
     hangman_list = [hangman_0, hangman_1, hangman_2, hangman_3, hangman_4, hangman_5, hangman_6, hangman_7, hangman_8]
     # printing hangman
@@ -107,7 +107,7 @@ def choose_category():
     print("Choose category: fruits, vegetables, animals, car_brands, countries")
     while True:
         user_input = input()
-        
+
         # returning numbers which will be indexes of list with categories
         if user_input == "fruits":
             return 0
@@ -129,46 +129,47 @@ def game():
     # categories and their contents
     # fruits list
     fruits = ["banana", "strawberry", "grape", "apple", "watermelon",
-    "orange", "blueberry", "lemon", "peach", "avocado", "cherry",
-    "cantaloupe", "raspberry", "pear", "lime", "blackberry", "clementine",
-    "mango", "plum"]
+              "orange", "blueberry", "lemon", "peach", "avocado", "cherry",
+              "cantaloupe", "raspberry", "pear", "lime", "blackberry", "clementine",
+              "mango", "plum"]
     # vegetables list
     vegetables = ["potato", "tomato", "onion", "carrot", "beetroot",
-    "broccoli", "cucumber", "salad", "lettuve", "celery", "mushroom",
-    "corn", "garlic", "spinach", "bean", "cabbage", "peas",
-    "green onion", "cauliflower", "asparagus"]
+                  "broccoli", "cucumber", "salad", "lettuce", "celery", "mushroom",
+                  "corn", "garlic", "spinach", "bean", "cabbage", "peas",
+                  "green onion", "cauliflower", "asparagus"]
     # animals list
     animals = ["cat", "dog", "rabbit", "horse", "chicken", "pig", "cow"
-    "sheep", "bird", "parrot", "frog", "spider", "bear", "snake", "hamster",
-    "monkey", "lion", "zebra", "shark", "fish"]
+                                                                  "sheep", "bird", "parrot", "frog", "spider", "bear",
+               "snake", "hamster",
+               "monkey", "lion", "zebra", "shark", "fish"]
     # car brands list
     car_brands = ["chevrolet", "honda", "nissan", "ford", "fiat", "jeep",
-    "volkswagen", "volvo", "jaguar", "audi", "toyota", "tesla", "lexus",
-    "porsche", "bugatti", "bentley", "hyundai", "bmw", "mercedes", "seat"]
+                  "volkswagen", "volvo", "jaguar", "audi", "toyota", "tesla", "lexus",
+                  "porsche", "bugatti", "bentley", "hyundai", "bmw", "mercedes", "seat"]
     # countries list
     countries = ["poland", "korea", "japan", "iceland", "england", "germany",
-    "ukraine", "france", "spain", "italy", "romania", "greece", "australia", "usa",
-    "canada", "mexico", "brazil", "argentina", "morocco", "egypt"]
+                 "ukraine", "france", "spain", "italy", "romania", "greece", "australia", "usa",
+                 "canada", "mexico", "brazil", "argentina", "morocco", "egypt"]
     # categories list
     categories = [fruits, vegetables, animals, car_brands, countries]
     # choosing category
     player_category = choose_category()
     # list of guessed letters
     guesses_list = []
-    
+
     # keeping track of results
     global games_won
     global games_lost
-    
+
     # selecting a random word
     word_to_guess = random.choice(categories[player_category])
     guessing_word = ((len(word_to_guess)) * "-")
     win_check = word_to_guess
     attempts = 8
-    
+
     # starting line
     print("H A N G M A N \n")
-    
+
     # game loop
     while attempts > 0:
         # checking if player guessed the word
@@ -177,21 +178,21 @@ def game():
             print("You survived!")
             games_won += 1
             break
-        
+
         # printing hangman
         print_hangman(attempts)
-        
+
         # printing previous guesses
         if len(guesses_list) > 0:
             print(f"Previous guesses: {guesses_list}")
-        
+
         # getting correct input
         guess = get_input(guessing_word)
-        
+
         # checking the word
         # case for guessing the correct letter
         if guess in word_to_guess:
-            # checking if multiple letters needs to be swaped
+            # checking if multiple letters needs to be swapped
             good_guesses_counter = word_to_guess.count(guess)
             for i in range(good_guesses_counter):
                 # swapping blank in word_to_guess with guessed letter
@@ -203,20 +204,20 @@ def game():
             print("You've already guessed this letter!")
             attempts -= 1
         else:
-        # case for guessing wrong letter
+            # case for guessing wrong letter
             print("That letter doesn't appear in the word!")
             attempts -= 1
-        
+
         # displaying the amount of attempts
         if attempts != 1:
-            print(f"# {attempts} attempts") 
+            print(f"# {attempts} attempts")
         else:
             print(f"# {attempts} attempt")
-            
+
         # adding guessed letter to list
         guesses_list.append(guess)
         print()
-        
+
     # case for when player runs out of attempts
     else:
         # printing hangman
@@ -238,30 +239,30 @@ def get_input(guessing_word):
         if len(letter) != 1:
             print("Please, input a single letter! \n")
         else:
-            # case for when letter isn't a lower case letter or a ASCI symbol
-            if (letter.isupper() or not ((letter >= "a") and (letter <= "z"))):
+            # case for when letter isn't a lower case letter or an ASCII symbol
+            if letter.isupper() or not ((letter >= "a") and (letter <= "z")):
                 print("Please, enter a lowercase letter from the English alphabet! \n")
             else:
                 return letter
 
 
 # printing results
-def print_results(games_won, games_lost):
-    # printing results for multiple or single won games 
-    if games_won == 1:
-        print(f"You won: {games_won} time")
+def print_results(won, lost):
+    # printing results for multiple or single won games
+    if won == 1:
+        print(f"You won: {won} time")
     else:
-        print(f"You won: {games_won} times")
-    # printing results for multiple or single lost games 
-    if games_lost == 1:
-       print(f"You lost: {games_lost} time")
+        print(f"You won: {won} times")
+    # printing results for multiple or single lost games
+    if lost == 1:
+        print(f"You lost: {lost} time")
     else:
-        print(f"You lost: {games_lost} times")
+        print(f"You lost: {lost} times")
 
 
 # replacing string at index wit given character
 def replace_string_at_index(word, character, index):
-    new_word = word[:index] + character + word[index+1:]
+    new_word = word[:index] + character + word[index + 1:]
     return new_word
 
 
@@ -273,10 +274,10 @@ def start_hangman():
         print("Type 'play' - to play the game")
         print("Type 'results' - to see the results")
         print("Type 'exit' - to exit the program")
-        
+
         # getting input
         user_input = input()
-        
+
         # choosing programs feature
         # case for starting the game
         if user_input == "play":
